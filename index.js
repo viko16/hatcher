ca27ef8686e62b0ecdb9dbf0049a38f54c12e65e
viko16 <16viko@gmail.com>
2018-01-22 00:08:35 +0800

    feat: throw if config object not validated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { URL } = require('url')
+const assert = require('assert')
 const PathParser = require('path-parser')
 const debug = require('debug')('hatcher:index')
 const { exec, match, parse } = require('matchit')
@@ -6,7 +7,9 @@ const { exec, match, parse } = require('matchit')
 const { crawlAndParse } = require('./lib/crawler')
 
 module.exports = function (config = {}) {
-  // TODO: check config
+  // check config
+  assert(config, '[hatcher:config] config must exist')
+  assert(config.entry, '[hatcher:config] config.entry must exist')
 
   return async function hatch (ctx, next) {
     const routes = Object.keys(config.entry).map(parse)
