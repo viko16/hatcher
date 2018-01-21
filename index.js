af4036cead64d51a22474b6cd7d9d3084408e6a4
viko16 <16viko@gmail.com>
2018-01-22 01:27:53 +0800

    feat: use koa-router

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { URL } = require('url')
 const assert = require('assert')
 const PathParser = require('path-parser')
 const debug = require('debug')('hatcher:index')
-const { exec, match, parse } = require('matchit')
+const Router = require('koa-router')
 
 const { crawlAndParse } = require('./lib/crawler')
 
@@ -11,26 +11,29 @@ module.exports = function (config = {}) {
   assert(config, '[hatcher:config] config must exist')
   assert(config.entry, '[hatcher:config] config.entry must exist')
 
-  return async function hatch (ctx, next) {
-    const routes = Object.keys(config.entry).map(parse)
-    const url = ctx.path
-    const arr = match(url, routes)
-    // missed
-    if (!arr.length) {
-      await next()
-      return
-    }
+  const router = new Router()
 
+  // index
+  router.get('/', async ctx => {
+    ctx.body = { routes: Object.keys(config.entry) }
+  })
+
+  for (const routeName in config.entry) {
     // matched
-    const key = arr[0].old
-    const params = exec(url, arr)
-
-    const itemConf = config.entry[key]
-    const targetPath = new PathParser(itemConf.path).build(params)
-    const targetUrl = new URL(targetPath, itemConf.baseUrl).toString()
-    debug('targetUrl', targetUrl)
-    // 爬取站点 + 解析内容
-    const result = await crawlAndParse(targetUrl, itemConf)
-    ctx.body = result
+    router.get(routeName, async ctx => {
+      try {
+        const itemConf = config.entry[routeName]
+        const targetPath = new PathParser(itemConf.path).build(ctx.params)
+        const targetUrl = new URL(targetPath, itemConf.baseUrl).toString()
+        debug('目标地址: %s', targetUrl)
+        // 爬取站点 + 解析内容
+        const result = await crawlAndParse(targetUrl, itemConf, config.setting)
+        ctx.body = result
+      } catch (error) {
+        ctx.body = { error: error.message }
+      }
+    })
   }
+
+  return router.routes()
 }
